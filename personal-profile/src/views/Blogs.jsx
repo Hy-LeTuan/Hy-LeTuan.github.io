09aac7b172aa55f63104b99cb0152ca31a3141cc
Hy-LeTuan <113957387+Hy-LeTuan@users.noreply.github.com>
2024-07-18 00:23:28 +0700

    update read more function on blogs + adds storage system

--- personal-profile/src/views/Blogs.jsx
+++ personal-profile/src/views/Blogs.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MediaHeader from "../layout/MediaHeader";
 import SquareAnimation from "../layout/SquareAnimation";
+import blogList from "../data/blogs/blogList.json";
+import categoryList from "../data/blogs/categories.json";
 
 function Blogs() {
+	const [blogs, setBlogs] = useState(blogList.blogs);
+	const [categories, setCategories] = useState(categoryList.categories);
+
+	console.log(categories);
+
+	const checkReadButton = (target) => {
+		if (target) {
+			if (target?.scrollHeight > target.clientHeight) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+
+	const showMoreContentOnClick = async (event) => {
+		const target = await event.target
+			.closest("button")
+			.parentNode.previousElementSibling.querySelector("p");
+
+		const img = await event.target.closest("button").querySelector("img");
+
+		target.classList.toggle("line-clamp-4");
+		img.classList.toggle("-rotate-90");
+	};
+
 	return (
 		<>
 			<section className="relative overflow-hidden clear-both w-full bg-primary text-white font-heading-font">
@@ -17,12 +45,17 @@ function Blogs() {
 					<h2 className="text-2xl font-heading-font text-black">
 						Categories
 					</h2>
-					<div className="flex gap-9 justify-center items-center">
-						<div className="bg-neutral w-9 h-9 rounded-full"></div>
-						<div className="bg-neutral w-9 h-9 rounded-full"></div>
-						<div className="bg-neutral w-9 h-9 rounded-full"></div>
-						<div className="bg-neutral w-9 h-9 rounded-full"></div>
-						<div className="bg-neutral w-9 h-9 rounded-full"></div>
+					<div className="flex flex-wrap gap-6 px-4 justify-between items-start">
+						{categories?.map((cat, index) => (
+							<div
+								className="flex flex-col items-center justify-center gap-2"
+								key={index}>
+								<div className="bg-neutral w-9 h-9 rounded-full"></div>
+								<h6 className="text-black font-heading-font text-sm">
+									{cat.title}
+								</h6>
+							</div>
+						))}
 					</div>
 				</div>
 				<div className="w-full p-2 bg-primary relative overflow-hidden mt-10 mb-10">
@@ -33,102 +66,46 @@ function Blogs() {
 				</div>
 				<div className="main-section mb-8">
 					<div className="card-holder">
-						<div className="blog-card">
-							<div className="w-full h-96 bg-neutral"></div>
-							<div className="blog-card-body">
-								<h2 className="text-2xl font-heading-font text-white text-center">
-									Blog title right here
-								</h2>
-								<p className="text-left font-body-font text-white text-xs">
-									Blog body, this is where all the contents
-									will sit. How do i know deal with the
-									imbalances? I’m not too sure, but I can set
-									a height limit, then anything after the
-									height limit can be an overlay and expanded
-									on top. It would be awesome to be able to
-									accomplish something like that.
-								</p>
-							</div>
-							<div className="flex w-full justify-between px-6 items-center mb-4">
-								<h6 className="text-sm font-heading-font text-white">
-									15/07/2024
-								</h6>
-								<button className="btn btn-card-footer gap-2">
+						{blogs?.map((blog, index) => (
+							<div className="blog-card" key={index}>
+								<div className="w-full h-96 bg-neutral"></div>
+								<div className="blog-card-body">
+									<h2 className="text-2xl font-heading-font text-white text-center">
+										{blog.title}
+									</h2>
+									<p
+										id={`blog-content-${index}`}
+										className="text-left font-body-font text-white text-xs line-clamp-4 transition-all duration-1000">
+										{blog.content}
+									</p>
+								</div>
+								<div className="flex w-full justify-between px-6 items-center mb-4">
 									<h6 className="text-sm font-heading-font text-white">
-										Read more
+										{blog.date}
 									</h6>
-									<img
-										src="./src/assets/expand.svg"
-										alt="expand icon"
-										className="text-white w-4 h-4"
-									/>
-								</button>
+									{checkReadButton(
+										document.querySelector(
+											`p[id="blog-content-${index}"]`
+										)
+									) && (
+										<button
+											className="btn btn-card-footer gap-2"
+											onClick={(event) =>
+												showMoreContentOnClick(event)
+											}>
+											<h6 className="text-sm font-heading-font text-white">
+												Read more
+											</h6>
+											<img
+												src="./src/assets/expand.svg"
+												alt="expand icon"
+												className="text-white w-4 h-4 -rotate-90 transition-all duration-300"
+											/>
+										</button>
+									)}
+								</div>
 							</div>
-						</div>
-						<div className="blog-card">
-							<div className="w-full h-96 bg-neutral"></div>
-							<div className="blog-card-body">
-								<h2 className="text-2xl font-heading-font text-white text-center">
-									Blog title right here
-								</h2>
-								<p className="text-left font-body-font text-white text-xs">
-									Blog body, this is where all the contents
-									will sit. How do i know deal with the
-									imbalances? I’m not too sure, but I can set
-									a height limit, then anything after the
-									height limit can be an overlay and expanded
-									on top. It would be awesome to be able to
-									accomplish something like that.
-								</p>
-							</div>
-							<div className="flex w-full justify-between px-6 items-center mb-4">
-								<h6 className="text-sm font-heading-font text-white">
-									15/07/2024
-								</h6>
-								<button className="btn btn-card-footer gap-2">
-									<h6 className="text-sm font-heading-font text-white">
-										Read more
-									</h6>
-									<img
-										src="./src/assets/expand.svg"
-										alt="expand icon"
-										className="text-white w-4 h-4"
-									/>
-								</button>
-							</div>
-						</div>
-						<div className="blog-card">
-							<div className="w-full h-96 bg-neutral"></div>
-							<div className="blog-card-body">
-								<h2 className="text-2xl font-heading-font text-white text-center">
-									Blog title right here
-								</h2>
-								<p className="text-left font-body-font text-white text-xs">
-									Blog body, this is where all the contents
-									will sit. How do i know deal with the
-									imbalances? I’m not too sure, but I can set
-									a height limit, then anything after the
-									height limit can be an overlay and expanded
-									on top. It would be awesome to be able to
-									accomplish something like that.
-								</p>
-							</div>
-							<div className="flex w-full justify-between px-6 items-center mb-4">
-								<h6 className="text-sm font-heading-font text-white">
-									15/07/2024
-								</h6>
-								<button className="btn btn-card-footer gap-2">
-									<h6 className="text-sm font-heading-font text-white">
-										Read more
-									</h6>
-									<img
-										src="./src/assets/expand.svg"
-										alt="expand icon"
-										className="text-white w-4 h-4"
-									/>
-								</button>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</section>
