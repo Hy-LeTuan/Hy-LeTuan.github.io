37241421285fe17ca78cf4838a5cdf62e69434ee
Hy-LeTuan <113957387+Hy-LeTuan@users.noreply.github.com>
2024-07-20 15:59:32 +0700

    update active specific for large screen size

--- personal-profile/src/views/ActiveSpecific.jsx
+++ personal-profile/src/views/ActiveSpecific.jsx
@@ -32,17 +32,17 @@ function ActiveSpecific() {
 	return (
 		<>
 			<section className="relative overflow-hidden clear-both w-full bg-primary text-white font-heading-font">
-				<div className="p-12 flex flex-col gap-6 justify-center items-center overflow-hidden">
-					<h1 className="text-3xl">Active Projects</h1>
+				<div className="p-12 flex flex-col gap-6 justify-center items-center overflow-hidden lg:p-24">
+					<h1 className="text-3xl lg:text-5xl">Active Projects</h1>
 					{MediaHeader("..")}
 					{SquareAnimation(null)}
 				</div>
 			</section>
-			<section className="mt-10">
+			<section className="mt-10 lg:mt-16">
 				<div className="main-section">
-					<div className="w-full flex flex-col justify-center items-center gap-8">
+					<div className="w-full flex flex-col justify-center items-center gap-8 lg:col-span-2">
 						<div className="rounded-full w-44 h-44 bg-gray-300 shadow-project mt-10"></div>
-						<h2 className="font-heading-font text-2xl">
+						<h2 className="font-heading-font text-2xl lg:text-4xl">
 							Project title: {activeProject?.title}
 						</h2>
 					</div>
@@ -50,88 +50,102 @@ function ActiveSpecific() {
 			</section>
 			<section className="mt-10">
 				<div className="main-section">
-					<div className="card">
+					<div className="card lg:col-span-2 lg:mb-0">
 						<div className="card-body-single-column gap-4">
 							<div className="project-specific-single-part">
 								<div className="flex items-center gap-2">
-									<h5 className="font-heading-font text-black text-base text-left">
+									<h5 className="font-heading-font text-black text-base text-left lg:text-xl">
 										Project description
 									</h5>
 									<img
-										className="w-5 h-5"
+										className="w-5 h-5 lg:w-6 lg:h-6"
 										src="../src/assets/description.svg"
 										alt="Description icon"
 									/>
 								</div>
-								<p className="text-left font-body-font text-black text-xs">
+								<p className="text-left font-body-font text-black text-xs lg:text-sm">
 									{activeProject?.description}
 								</p>
 							</div>
-							<div className="project-specific-single-part">
-								<div className="flex items-center gap-2">
-									<h5 className="font-heading-font text-black text-base text-left">
-										Technologies involved
-									</h5>
-									<img
-										className="w-5 h-5"
-										src="../src/assets/list_heart.svg"
-										alt="Description icon"
-									/>
+							<div className="hidden lg:flex lg:flex-row lg:items-start lg:justify-center lg:gap-8">
+								<div className="hidden lg:flex lg:flex-col lg:w-1/3 lg:gap-4">
+									<div className="project-specific-single-part">
+										<div className="flex items-center gap-2">
+											<h5 className="font-heading-font text-black text-base text-left lg:text-xl">
+												Technologies involved
+											</h5>
+											<img
+												className="w-5 h-5 lg:w-6 lg:h-6"
+												src="../src/assets/list_heart.svg"
+												alt="Description icon"
+											/>
+										</div>
+										<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-black list-disc list-inside marker:text-accent lg:text-sm lg:gap-2">
+											{activeProject?.technologies?.map(
+												(tech, index) => (
+													<li key={index}>{tech}</li>
+												)
+											)}
+										</ul>
+									</div>
+									<div className="project-specific-single-part">
+										<div className="flex items-center gap-2">
+											<h5 className="font-heading-font text-black text-base text-left lg:text-xl">
+												Project statistics
+											</h5>
+											<img
+												className="w-5 h-5 lg:w-6 lg:h-6"
+												src="../src/assets/statistics.svg"
+												alt="Description icon"
+											/>
+										</div>
+										<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-black list-disc list-inside marker:text-accent lg:text-sm lg:gap-2">
+											<li>
+												Date started:{" "}
+												{
+													activeProject?.statistics
+														?.date
+												}
+											</li>
+											<li>
+												Number of collaborators:{" "}
+												{
+													activeProject?.statistics
+														?.collaborators
+												}
+											</li>
+											<li>
+												Project length:{" "}
+												{
+													activeProject?.statistics
+														?.length
+												}
+												h
+											</li>
+											<li>
+												Project status:{" "}
+												{
+													activeProject?.statistics
+														?.status
+												}
+											</li>
+										</ul>
+									</div>
 								</div>
-								<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-black list-disc list-inside marker:text-accent">
-									{activeProject?.technologies?.map(
-										(tech, index) => (
-											<li key={index}>{tech}</li>
-										)
-									)}
-								</ul>
+								<div className="w-full h-60 bg-primary lg:h-60 lg:w-2/3 lg:flex-grow"></div>
 							</div>
-							<div className="project-specific-single-part">
-								<div className="flex items-center gap-2">
-									<h5 className="font-heading-font text-black text-base text-left">
-										Project statistics
-									</h5>
-									<img
-										className="w-5 h-5"
-										src="../src/assets/statistics.svg"
-										alt="Description icon"
-									/>
-								</div>
-								<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-black list-disc list-inside marker:text-accent">
-									<li>
-										Date started:{" "}
-										{activeProject?.statistics?.date}
-									</li>
-									<li>
-										Number of collaborators:{" "}
-										{
-											activeProject?.statistics
-												?.collaborators
-										}
-									</li>
-									<li>
-										Project length:{" "}
-										{activeProject?.statistics?.length}h
-									</li>
-									<li>
-										Project status:{" "}
-										{activeProject?.statistics?.status}
-									</li>
-								</ul>
-							</div>
-							<div className="w-full h-60 bg-primary"></div>
 						</div>
 					</div>
 				</div>
 			</section>
 			{SectionBanner(5, "Goals & Aims")}
-			<section className="mb-8">
+			<section className="mb-8 lg:mb-14">
 				<div className="main-section">
 					<div className="card-holder">
 						{activeProject?.goals_and_aims?.map((target, index) => (
 							<div className="target-card" key={index}>
 								<div className="flex justify-start items-center">
-									<h4 className="text-xl text-white font-heading-font mr-5">
+									<h4 className="text-xl text-white font-heading-font mr-5 lg:text-2xl">
 										{target.title}
 									</h4>
 									<img
@@ -143,7 +157,7 @@ function ActiveSpecific() {
 								<hr className="w-full text-white" />
 								<div className="project-specific-single-part">
 									<div className="flex items-center gap-2">
-										<h5 className="font-heading-font text-white text-base text-left">
+										<h5 className="font-heading-font text-white text-base text-left lg:text-xl">
 											Target description
 										</h5>
 										<img
@@ -152,16 +166,16 @@ function ActiveSpecific() {
 											alt="Description icon"
 										/>
 									</div>
-									<p className="text-left font-body-font text-white text-xs mt-2">
+									<p className="text-left font-body-font text-white text-xs mt-2 lg:text-sm">
 										{target.description}
 									</p>
 								</div>
 								<div className="target-card-footer">
 									<div className="target-card-footer-col">
-										<h5 className="font-heading-font text-white text-base text-left">
+										<h5 className="font-heading-font text-white text-base text-left lg:text-xl">
 											Target deadline
 										</h5>
-										<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-white list-disc list-inside marker:text-white">
+										<ul className="flex flex-col gap-1 font-body-font text-xs text-left text-white list-disc list-inside marker:text-white lg:text-sm">
 											<li>
 												Duration:{" "}
 												{target.deadline?.duratin}
@@ -173,10 +187,10 @@ function ActiveSpecific() {
 										</ul>
 									</div>
 									<div className="target-card-footer-col">
-										<h5 className="font-heading-font text-white text-base text-left">
+										<h5 className="font-heading-font text-white text-base text-left lg:text-xl">
 											Status
 										</h5>
-										<p className="text-left font-body-font text-white text-xs">
+										<p className="text-left font-body-font text-white text-xs lg:text-sm">
 											{target.status}
 										</p>
 									</div>
