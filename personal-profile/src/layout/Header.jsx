f16863ba273a10e57f30088a4419902bc226fc0d
Hy-LeTuan <113957387+Hy-LeTuan@users.noreply.github.com>
2024-07-31 23:22:27 +0700

    update navbar dropdown menu

--- personal-profile/src/layout/Header.jsx
+++ personal-profile/src/layout/Header.jsx
@@ -23,47 +23,6 @@ function Header() {
 		<nav className={`relative z-10 w-full bg-primary ${shadowStyle}`}>
 			<div className="flex justify-between items-center px-6 py-2">
 				<h6 className="text-white">Le Tuan Hy</h6>
-
-				{/* Button container */}
-				{/* <div className="flex justify-between items-center px-6 py-1 md:p-0 md:justify-center md:gap-10">
-					<Link
-						to={"/"}
-						className={
-							isActiveLink(location.pathname, "/") &&
-							location.pathname.length == 1
-								? "btn btn-header-active"
-								: "btn btn-header"
-						}>
-						<h6>Home</h6>
-					</Link>
-					<Link
-						to={"/portfolio"}
-						className={
-							isActiveLink(location.pathname, "/portfolio")
-								? "btn btn-header-active"
-								: "btn btn-header"
-						}>
-						<h6>Profile</h6>
-					</Link>
-					<Link
-						to={"/active"}
-						className={
-							isActiveLink(location.pathname, "/active")
-								? "btn btn-header-active"
-								: "btn btn-header"
-						}>
-						<h6>Active</h6>
-					</Link>
-					<Link
-						to={"/blogs"}
-						className={
-							isActiveLink(location.pathname, "/blogs")
-								? "btn btn-header-active"
-								: "btn btn-header"
-						}>
-						<h6>Blogs</h6>
-					</Link>
-				</div> */}
 				<Menu>
 					<MenuButton className="flex flex-col gap-1 group">
 						<img
@@ -81,11 +40,39 @@ function Header() {
 					<MenuItems
 						transition
 						anchor="bottom end"
-						className="w-20 origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-white transition duration-100 ease-out [--anchor-gap:10px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 z-20">
+						className="w-32 origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-white transition duration-100 ease-out [--anchor-gap:10px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 z-20">
 						<MenuItem>
-							<button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-								Edit
-							</button>
+							<Link
+								className={
+									isActiveLink(location.pathname, "/") &&
+									location.pathname.length == 1
+										? "group flex w-full items-center justify-center gap-3 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10 underline underline-offset-4"
+										: "group flex w-full items-center justify-center gap-3 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+								}
+								to={"/"}>
+								<img
+									src="/src/assets/home.svg"
+									alt=""
+									className="w-3 h-3"
+								/>
+								<h6>Home</h6>
+							</Link>
+						</MenuItem>
+						<div className="my-1 h-px bg-white/5"></div>
+						<MenuItem>
+							<Link
+								className={
+									isActiveLink(location.pathname, "/profile")
+										? "group flex w-full items-center justify-center gap-3 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10 underline underline-offset-4"
+										: "group flex w-full items-center justify-center gap-3 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+								}>
+								<img
+									src="/src/assets/profile.svg"
+									alt=""
+									className="w-4 h-4"
+								/>
+								<h6>Profile</h6>
+							</Link>
 						</MenuItem>
 					</MenuItems>
 				</Menu>
